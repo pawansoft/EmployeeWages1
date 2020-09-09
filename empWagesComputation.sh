8f027470c6fc6ab3343518f35f79834791f54243
Pawan Kumar <pk.soft29@gmail.com>
2020-09-09 20:35:09 +0530

    Added: Half time funcnality added into EmployeeWagesComputation.sh

--- empWagesComputation.sh
+++ empWagesComputation.sh
@@ -4,13 +4,19 @@ WAGE_PER_HOUR=20;
 FULL_DAY_HOUR=8;
 DAILY_WAGE=0
 isPresent=1;
+HALF_TIME=4;
 
-attendance=$((RANDOM%2))
+attendance=$((RANDOM%3))
 
 if [ $attendance -eq $isPresent ]
 then
 	DAILY_WAGE=$((DAILY_WAGE + $WAGE_PER_HOUR * $FULL_DAY_HOUR))
 	echo "Employee is present"
+elif [ $attendance -eq 2 ]
+then
+	echo "Half Time"
+	DAILY_WAGE=$((DAILY_WAGE * $HALF_TIME))
+
 else
 	echo "Employee is Absent" 
 fi
