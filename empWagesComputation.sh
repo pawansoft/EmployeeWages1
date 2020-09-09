37bda3aa3a19454a33b8a86ce45d789e8308e341
Pawan Kumar <pk.soft29@gmail.com>
2020-09-09 20:24:28 +0530

    Added: Daily Wage Employee Calculation funcnality added

--- empWagesComputation.sh
+++ empWagesComputation.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-
+WAGE_PER_HOUR=20;
+FULL_DAY_HOUR=8;
+DAILY_WAGE=0
 isPresent=1;
 
 attendance=$((RANDOM%2))
 
 if [ $attendance -eq $isPresent ]
 then
+	DAILY_WAGE=$((DAILY_WAGE + $WAGE_PER_HOUR * $FULL_DAY_HOUR))
 	echo "Employee is present"
 else
 	echo "Employee is Absent" 
 fi
+
+echo "Daily wage = $DAILY_WAGE"
