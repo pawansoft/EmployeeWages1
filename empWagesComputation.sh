7961316bc4d0bc88ecfe80fac0adfe3b198ae0b6
Pawan Kumar <pk.soft29@gmail.com>
2020-09-09 20:44:45 +0530

    Added:  EmployeeWagesComputation.sh solved using switch Case Statment

--- empWagesComputation.sh
+++ empWagesComputation.sh
@@ -3,22 +3,22 @@
 WAGE_PER_HOUR=20;
 FULL_DAY_HOUR=8;
 DAILY_WAGE=0
-isPresent=1;
 HALF_TIME=4;
 
 attendance=$((RANDOM%3))
-
-if [ $attendance -eq $isPresent ]
-then
+case $attendance in
+1)
 	DAILY_WAGE=$((DAILY_WAGE + $WAGE_PER_HOUR * $FULL_DAY_HOUR))
 	echo "Employee is present"
-elif [ $attendance -eq 2 ]
-then
+	;;
+2)
 	echo "Half Time"
-	DAILY_WAGE=$((DAILY_WAGE * $HALF_TIME))
+	DAILY_WAGE=$((DAILY_WAGE + $HALF_TIME * $WAGE_PER_HOUR))
+	;;
 
-else
-	echo "Employee is Absent" 
-fi
+*)
+	echo "Employee is Absent"
+	;; 
+esac
 
 echo "Daily wage = $DAILY_WAGE"
