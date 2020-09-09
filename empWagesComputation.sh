13931a0607d7eee974d622e944bcbf628fb2d0df
Pawan Kumar <pk.soft29@gmail.com>
2020-09-09 20:52:57 +0530

    Added:  EmployeeWagesComputation.sh with calculation of wages for month funcnality

--- empWagesComputation.sh
+++ empWagesComputation.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
-
+WORKDAY_PER_MONTH=20;
 WAGE_PER_HOUR=20;
 FULL_DAY_HOUR=8;
 DAILY_WAGE=0
 HALF_TIME=4;
 
-attendance=$((RANDOM%3))
-case $attendance in
-1)
-	DAILY_WAGE=$((DAILY_WAGE + $WAGE_PER_HOUR * $FULL_DAY_HOUR))
-	echo "Employee is present"
-	;;
-2)
-	echo "Half Time"
-	DAILY_WAGE=$((DAILY_WAGE + $HALF_TIME * $WAGE_PER_HOUR))
-	;;
+for (( count = 1; count <=  $WORKDAY_PER_MONTH; count++ ))
+do
 
-*)
-	echo "Employee is Absent"
-	;; 
-esac
+	attendance=$((RANDOM%3))
+	case $attendance in
+	1)
+		DAILY_WAGE=$((DAILY_WAGE + $WAGE_PER_HOUR * $FULL_DAY_HOUR))
+		echo "Employee is present"
+		;;
+	2)
+		echo "Half Time"
+		DAILY_WAGE=$((DAILY_WAGE + $HALF_TIME * $WAGE_PER_HOUR))
+		;;
 
+	*)
+		echo "Employee is Absent"
+		;; 
+	esac
+done
 echo "Daily wage = $DAILY_WAGE"
