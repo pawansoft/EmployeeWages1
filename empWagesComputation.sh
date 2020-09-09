d30fc2e14f572f5ffee0a0c882fce97940998413
Pawan Kumar <pk.soft29@gmail.com>
2020-09-09 21:04:33 +0530

    Added:  EmployeeWagesComputation.sh with condition of total hour or day reached for a month

--- empWagesComputation.sh
+++ empWagesComputation.sh
@@ -4,8 +4,9 @@ WAGE_PER_HOUR=20;
 FULL_DAY_HOUR=8;
 DAILY_WAGE=0
 HALF_TIME=4;
-
-for (( count = 1; count <=  $WORKDAY_PER_MONTH; count++ ))
+day=1
+hour=0
+while [[ $day -lt 20 && $hour -lt 100 ]]
 do
 
 	attendance=$((RANDOM%3))
@@ -13,10 +14,12 @@ do
 	1)
 		DAILY_WAGE=$((DAILY_WAGE + $WAGE_PER_HOUR * $FULL_DAY_HOUR))
 		echo "Employee is present"
+		day=$((day+1))
 		;;
 	2)
 		echo "Half Time"
 		DAILY_WAGE=$((DAILY_WAGE + $HALF_TIME * $WAGE_PER_HOUR))
+		hour=$((hour+4))
 		;;
 
 	*)
@@ -25,3 +28,5 @@ do
 	esac
 done
 echo "Daily wage = $DAILY_WAGE"
+echo "Full Day: $day"
+echo "Part Time: $hour"
